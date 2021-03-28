7fbf55215e0c1201623803244c4622d5324d2b69
Josh Kraker <joshkrak@gmail.com>
2021-03-28 09:48:03 -0400

    Parsing child object

--- src/transformer.ts
+++ src/transformer.ts
@@ -18,7 +18,7 @@ export function transformer(program: ts.Program): ts.TransformerFactory<ts.Sourc
             const symbols = typeChecker.getPropertiesOfType(type);
             const schemaProperties = symbols.map(s => createField(s, typeChecker));
 
-            console.log(schemaProperties);
+            console.log(JSON.stringify(schemaProperties, null, 3));
 
          }
 
@@ -35,11 +35,20 @@ function createField(symbol: ts.Symbol, typeChecker: ts.TypeChecker) {
    return {
       name: symbol.getName(),
       allowUndefined: symbol.declarations.some(d => (d as ts.PropertySignature).questionToken),
-      type: getType(symbol, typeChecker)
+      allowNull: isNullable(symbol),
+      type: getTypes(symbol, typeChecker)
    }
 }
 
-function getType(symbol: ts.Symbol, typeChecker: ts.TypeChecker) {
+function isNullable(symbol: ts.Symbol): boolean {
+   const declarations = symbol.declarations;
+   const propertySignatures: ts.PropertySignature[] = declarations.filter(d => ts.isPropertySignature(d)) as ts.PropertySignature[];
+   const unionNodes: ts.UnionTypeNode[] = propertySignatures.filter(p => p.type && ts.isUnionTypeNode(p.type)).map(p => p.type!) as ts.UnionTypeNode[];
+   if (unionNodes.some(u => u.types.some(t => ts.isLiteralTypeNode(t) && t.getChildren()[0].kind === ts.SyntaxKind.NullKeyword))) { return true; }
+   return false;
+}
+
+function getTypes(symbol: ts.Symbol, typeChecker: ts.TypeChecker) {
 
    const fullName = `${(symbol as any).parent?.name}.${symbol.name}`;
 
@@ -51,14 +60,44 @@ function getType(symbol: ts.Symbol, typeChecker: ts.TypeChecker) {
 
    if (!ts.isPropertySignature(declaration)) { throw new Error(`Declaration for ${fullName} was not a PropertyDeclaration`); }
 
-   const type = declaration.type;
-
-   if (!type) { throw new Error(`No type given for ${fullName}`); }
-
-   switch (type.kind) {
-      case ts.SyntaxKind.NumberKeyword: return Number;
-      case ts.SyntaxKind.StringKeyword: return String;
-      default: throw new Error(`Property type ${ts.SyntaxKind[type.kind]} for ${fullName} not supported`);
+   if (!declaration.type) {
+      throw new Error(`Declaration for ${fullName} did not have a type`);
    }
 
+   let types = [declaration.type];
+
+
+   if (ts.isUnionTypeNode(types[0])) {
+      types = [...(types[0] as ts.UnionTypeNode).types];
+   }
+
+   const returnTypes: (string | Record<string, any>[])[] = [];
+
+   for (const t of types) {
+      const retType = getType(fullName, t, typeChecker);
+      if (!retType) { continue; }
+      returnTypes.push(retType);
+   }
+
+   return returnTypes;
+
 }
+
+function getType(fullName: string, t: ts.TypeNode, typeChecker: ts.TypeChecker) {
+   if (ts.isLiteralTypeNode(t) && t.getChildren()[0].kind === ts.SyntaxKind.NullKeyword) { return undefined; }
+
+   switch (t.kind) {
+      case ts.SyntaxKind.NumberKeyword: return 'number';
+      case ts.SyntaxKind.StringKeyword: return 'string';
+      case ts.SyntaxKind.BooleanKeyword: return 'boolean';
+      case ts.SyntaxKind.TypeReference: {
+         const refType = t as ts.TypeReferenceNode;
+         const type = typeChecker.getTypeFromTypeNode(refType);
+         const typeProps = typeChecker.getPropertiesOfType(type);
+         const fields = typeProps.map(p => createField(p, typeChecker));
+         return fields;
+      }
+      default: throw new Error(`Property type ${ts.SyntaxKind[t.kind]} for ${fullName} not supported`);
+   }
+}
+
--- test/ExampleModel.ts
+++ test/ExampleModel.ts
@@ -3,6 +3,16 @@ export interface ExampleModel {
    id: string;
    created: number;
    name: string;
-   description: string;
+   description: string | null;
    imUndefinable?: string;
+   inner: InnerModel | null;
+}
+
+export interface InnerModel {
+
+   id: string;
+
+   hasGood: boolean;
+
+
 }
